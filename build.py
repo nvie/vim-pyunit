fe7783bc695bfa546dbddd1530177837dff67e31
Vincent Driessen <vincent@datafox.nl>
2010-07-30 13:10:36 +0200

    Create directory if it does not exist yet.

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+import os
 
 def build():
     py_src = file('src/python_unittests.py').read()
     vim_src = file('src/python_unittests.vim').read()
     combined_src = vim_src.replace('__PYTHON_SOURCE__', py_src)
+    if not os.path.exists('ftplugin'):
+        os.mkdir('ftplugin')
     file('ftplugin/python_unittests.vim', 'w').write(combined_src)
 
 if __name__ == '__main__':
