bdfdfa6b327f7d0ea967f2f8f077cced9a088041
Vincent Driessen <vincent@datafox.nl>
2010-07-30 19:45:19 +0200

    Rename source files.

--- build.py
+++ build.py
@@ -3,11 +3,11 @@ import os
 
 def build():
     py_src = file('src/python_unittests.py').read()
-    vim_src = file('src/python_unittests.vim').read()
+    vim_src = file('src/base.vim').read()
     combined_src = vim_src.replace('__PYTHON_SOURCE__', py_src)
     if not os.path.exists('ftplugin'):
         os.mkdir('ftplugin')
-    file('ftplugin/python_unittests.vim', 'w').write(combined_src)
+    file('ftplugin/python_pyunit.vim', 'w').write(combined_src)
 
 if __name__ == '__main__':
     build()
