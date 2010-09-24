3967a20526aff3b4c1b7c7a7782b9d8ff5a8bdab
Vincent Driessen <vincent@datafox.nl>
2010-09-24 17:27:40 +0200

    Remove manual folds and restructure test methods.

--- tests/test_python_unittests.py
+++ tests/test_python_unittests.py
@@ -52,25 +52,35 @@ class TestSomething(unittest.TestCase):
         self.assertEquals(vim.eval('g:PyUnitTestPrefix'), 'test_')
         self.assertEquals(vim.eval('g:PyUnitShowTests'), '1')
 
-    # }}}
 
 
-    def test_is_test_file(self):  # {{{
-        self.assertTrue(mod.is_test_file('tests/foo.py'))
-
-        vimvar['g:PyUnitTestsRoot'] = 'my_tests'
-        self.assertFalse(mod.is_test_file('tests/foo.py'))
-        # }}}
-
-    def test_is_fs_root(self):  # {{{
+    def test_is_fs_root(self):
         self.assertTrue(mod.is_fs_root('/'))
         self.assertFalse(mod.is_fs_root(''))
         self.assertTrue(mod.is_fs_root(os.path.expandvars('$HOME')))
         vimvar['g:ProjRootStopAtHomeDir'] = '0'
         self.assertFalse(mod.is_fs_root(os.path.expandvars('$HOME')))
-        # }}}
 
-    def test_relpath(self):  # {{{
+    def test_find_project_root(self):
+        self.assertEquals(mod.find_project_root(currfile), proj_root)
+
+    def test_source_root(self):
+        self.assertEquals(mod.find_source_root(currfile),
+                          proj_root + '/')
+        vimvar['g:PyUnitSourceRoot'] = 'src'
+        self.assertEquals(mod.find_source_root(currfile),
+                          os.path.join(proj_root, 'src'))
+
+        vimvar['g:PyUnitSourceRoot'] = 'alt-src'
+        self.assertEquals(mod.find_source_root(currfile),
+                          os.path.join(proj_root, 'alt-src'))
+
+        vimvar['g:PyUnitSourceRoot'] = ''
+        self.assertEquals(mod.find_source_root(currfile),
+                          os.path.join(proj_root, ''))
+
+
+    def test_relpath(self):
         # Nice and simple
         self.assertEquals(
                 mod._relpath('/tmp/foo/bar', '/tmp', try_stdlib=False),
@@ -100,26 +110,59 @@ class TestSomething(unittest.TestCase):
                 mod._relpath('foo', 'foo/', try_stdlib=False), '.')
         self.assertEquals(
                 mod._relpath('foo', 'foo', try_stdlib=False), '.')
-        # }}}
 
-    def test_find_project_root(self):  # {{{
-        self.assertEquals(mod.find_project_root(currfile), proj_root)
-        # }}}
 
-    def test_source_root(self):  # {{{
-        self.assertEquals(mod.find_source_root(currfile),
-                          proj_root + '/')
-        vimvar['g:PyUnitSourceRoot'] = 'src'
-        self.assertEquals(mod.find_source_root(currfile),
-                          os.path.join(proj_root, 'src'))
-        # }}}
+    def test_is_test_file(self):
+        self.assertTrue(mod.is_test_file('tests/foo.py'))
 
-    def test_source_root_for_non_source_file(self):  # {{{
+        vimvar['g:PyUnitTestsRoot'] = 'my_tests'
+        self.assertFalse(mod.is_test_file('tests/foo.py'))
+
+    def test_source_root_for_non_source_file(self):
         self.assertRaises(Exception,
                 mod.find_source_root, '/tmp/foo.py')
-        # }}}
 
-    def test_vim_split_cmd(self):  # {{{
+    def test_get_test_file_for_normal_source_file(self):
+        self.assertSameFile(
+                mod.get_test_file_for_source_file('foo/bar/qux.py'),
+                'tests/test_foo/test_bar/test_qux.py')
+
+        vimvar['g:PyUnitTestsRoot'] = 'misc/mytests'
+        self.assertSameFile(
+                mod.get_test_file_for_source_file('foo/bar/qux.py'),
+                'misc/mytests/test_foo/test_bar/test_qux.py')
+
+        vimvar['g:PyUnitTestsStructure'] = 'flat'
+        self.assertSameFile(
+                mod.get_test_file_for_source_file('foo/bar/qux.py'),
+                'misc/mytests/test_foo_bar_qux.py')
+
+    def test_get_test_file_for_init_source_file(self):
+        self.assertSameFile(
+                mod.get_test_file_for_source_file('foo/bar/__init__.py'),
+                'tests/test_foo/test_bar.py')
+
+        vimvar['g:PyUnitTestsRoot'] = 'misc/mytests'
+        self.assertSameFile(
+                mod.get_test_file_for_source_file('foo/bar/__init__.py'),
+                'misc/mytests/test_foo/test_bar.py')
+
+        vimvar['g:PyUnitTestsStructure'] = 'flat'
+        self.assertSameFile(
+                mod.get_test_file_for_source_file('foo/bar/__init__.py'),
+                'misc/mytests/test_foo_bar.py')
+
+    def test_get_source_file_for_test_file(self):
+        self.assertRaises(Exception,
+                mod.find_source_file_for_test_file, currfile)
+
+        vimvar['g:PyUnitSourceRoot'] = 'src'
+        self.assertSameFile(
+                mod.find_source_file_for_test_file('tests/test_python_unittests.py'),
+                os.path.realpath('src/python_unittests.py'))
+
+
+    def test_vim_split_cmd(self):
         self.assertEquals(mod._vim_split_cmd(), 'vert rightb')
         self.assertEquals(mod._vim_split_cmd(True), 'vert lefta')
 
@@ -134,9 +177,8 @@ class TestSomething(unittest.TestCase):
         vimvar['g:PyUnitTestsSplitWindow'] = 'bottom'
         self.assertEquals(mod._vim_split_cmd(True), 'lefta')
         self.assertEquals(mod._vim_split_cmd(), 'rightb')
-        # }}}
 
-    def test_open_buffer_cmd(self):  # {{{
+    def test_open_buffer_cmd(self):
         vimvar['bufexists("foo")'] = '1'
         self.assertEquals(mod._open_buffer_cmd('foo'),
                 'vert rightb sbuffer foo')
@@ -150,46 +192,3 @@ class TestSomething(unittest.TestCase):
         vimvar['g:PyUnitTestsSplitWindow'] = 'no'
         self.assertEquals(mod._open_buffer_cmd('foo'),
                 'edit foo')
-        # }}}
-
-    def test_get_test_file_for_normal_source_file(self):  # {{{
-        self.assertSameFile(
-                mod.get_test_file_for_source_file('foo/bar/qux.py'),
-                'tests/test_foo/test_bar/test_qux.py')
-
-        vimvar['g:PyUnitTestsRoot'] = 'misc/mytests'
-        self.assertSameFile(
-                mod.get_test_file_for_source_file('foo/bar/qux.py'),
-                'misc/mytests/test_foo/test_bar/test_qux.py')
-
-        vimvar['g:PyUnitTestsStructure'] = 'flat'
-        self.assertSameFile(
-                mod.get_test_file_for_source_file('foo/bar/qux.py'),
-                'misc/mytests/test_foo_bar_qux.py')
-        # }}}
-
-    def test_get_test_file_for_init_source_file(self):  # {{{
-        self.assertSameFile(
-                mod.get_test_file_for_source_file('foo/bar/__init__.py'),
-                'tests/test_foo/test_bar.py')
-
-        vimvar['g:PyUnitTestsRoot'] = 'misc/mytests'
-        self.assertSameFile(
-                mod.get_test_file_for_source_file('foo/bar/__init__.py'),
-                'misc/mytests/test_foo/test_bar.py')
-
-        vimvar['g:PyUnitTestsStructure'] = 'flat'
-        self.assertSameFile(
-                mod.get_test_file_for_source_file('foo/bar/__init__.py'),
-                'misc/mytests/test_foo_bar.py')
-        # }}}
-
-    def test_get_source_file_for_test_file(self):  # {{{
-        self.assertRaises(Exception,
-                mod.find_source_file_for_test_file, currfile)
-
-        vimvar['g:PyUnitSourceRoot'] = 'src'
-        self.assertSameFile(
-                mod.find_source_file_for_test_file('tests/test_python_unittests.py'),
-                os.path.realpath('src/python_unittests.py'))
-        # }}}
